64173879cecb0383bb87953d0731e71d35b5926f
Marcelo Filho <marcelo.leda@icomp.ufam.edu.br>
2023-07-26 08:01:51 -0400

    feat: RF10

--- app/Controllers/Http/RoomsController.ts
+++ app/Controllers/Http/RoomsController.ts
@@ -5,8 +5,8 @@ import Room from 'App/Models/Room'
 export default class RoomsController {
     public async store({request,params, response}:HttpContextContract){
         const body= request.body()
-        const professorId = params.registration
-        const professor = await Professor.findByOrFail('registration_number', professorId);
+        const registration = params.registration
+        const professor = await Professor.findByOrFail('registration_number', registration);
 
         const createRoom = {...body, professor_id: professor.id}
         console.log(createRoom)
@@ -17,6 +17,27 @@ export default class RoomsController {
         return {
             message: "room created",
             data: room
+        }
+    }
+    public async update({params, request, response}:HttpContextContract){
+        const body = request.body()
+        const {registration, id} = params
+
+        const professor = await Professor.findByOrFail('registration_number', registration);
+        const room = await Room.findByOrFail("room_number", id)
+        if(professor.id !== room.professor_id){
+            response.status(401)
+            return
+        }
+
+        room.room_number = body.room_number
+        room.capacity = body.capacity
+        room.is_avaliable = body.is_avaliable
+
+        await room.save()
+        return {
+            message: "updated",
+            data: room,
         }
     }
 }
--- start/routes.ts
+++ start/routes.ts
@@ -37,5 +37,6 @@ Route.group(()=>{
   Route.resource("/students", "StudentsController").apiOnly()
   Route.resource("/professors", "ProfessorsController").apiOnly()
   Route.post("/rooms/:registration/professor", "RoomsController.store")
+  Route.put("/rooms/:registration/professor/:id", "RoomsController.update")
 
 }).prefix('/api')
